57ec6024d5f48b434b6786be52bd573092a4e32a
Muelsyseqwq <1615844471@qq.com>
2026-04-24 11:02:06 +0800

    finish HW07

---
+++ HW/hw07/hw07.scm
@@ -0,0 +1,14 @@
+(define (square n) (* n n))
+
+(define (pow base exp) (if (zero? exp) 1  (if (even? exp) (square (pow base (/ exp 2))) (* base (pow base (- exp 1))))))
+
+(define (repeatedly-cube n x)
+  (if (zero? n)
+      x
+      (begin (define y (pow x 3)) (repeatedly-cube (- n 1) y))))
+
+(define (cddr s) (cdr (cdr s)))
+
+(define (cadr s) (car (cdr s)))
+
+(define (caddr s) (car (cddr s)))
